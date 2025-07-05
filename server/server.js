a329927931d0001c1501d5438fa79882c1f75558
Nikunj Agarwal <nikunjagarwal1704@outlook.com>
2025-07-06 00:50:34 +0530

    frontend link update

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173','https://animation-club.vercel.app/'];
 
 // Security and utility middleware
 app.use(apiRateLimit);
