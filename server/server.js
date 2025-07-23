6cbc00cf882deeba3d708b33b4c976e26726df56
Nikunj Agarwal <nikunjagarwal1704@outlook.com>
2025-07-23 17:47:22 +0530

    patch added

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.use(cors({
         }
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 
